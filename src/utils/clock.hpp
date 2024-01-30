e29da8d4b28d07d615c8fe1e091b1e6e76864c3d
tkoz0 <13438573+tkoz0@users.noreply.github.com>
2024-01-29 19:47:02 -0500

    switched to header only

--- src/utils/clock.hpp
+++ src/utils/clock.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <cstdlib>
+#include <ctime>
 
 // returns the nanosecond (or most precise) performance counter
-size_t clock_nanotime();
+size_t clock_nanotime()
+{
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC,&t);
+    return 1000000000uLL*t.tv_sec + t.tv_nsec;
+}
